5e3204bb782b90418ef1c94a8620010002065351
korzeniowska18 <57856955+korzeniowska18@users.noreply.github.com>
2020-05-14 12:06:49 +0200

    update test case

--- Next_Test_cases/test_search_product.py
+++ Next_Test_cases/test_search_product.py
@@ -1,6 +1,11 @@
+
+
 from selenium import webdriver
 from page_object_pattern.pages.amis_home_page import AmisHomePage
 from page_object_pattern.pages.amis_result_page import AmisResultPage
+from page_object_pattern.pages.amis_search_page import AmisSearchPage
+from page_object_pattern.pages.amis_add_to_cart_page import AmisAddToCartPage
+from page_object_pattern.pages.amis_alert_message_page import AmisAlertMessagePage
 
 
 
@@ -23,5 +28,12 @@ class TestAmisSearch:
         home_page.search_in_amis("waga")
         amis_search_result = AmisResultPage(self.driver)
         amis_search_result.open_first_product()
+        amis_search_product = AmisSearchPage(self.driver)
+        amis_search_product.search_product()
+        add_product_to_cart = AmisAddToCartPage(self.driver)
+        add_product_to_cart.add_to_cart()
+        alert_message_appears = AmisAlertMessagePage(self.driver)
+        alert_message_appears.alert_message_product_add_to_cart()
 
+        print("Test case 'TestSearchProduct' completed")
 
