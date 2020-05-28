0596b0f55f1c41324ed840fab2e52a2021e72d15
korzeniowska18 <57856955+korzeniowska18@users.noreply.github.com>
2020-05-28 07:15:34 +0200

    create test case

---
+++ Test_Cases/test_case_1_add_to_cart.py
@@ -0,0 +1,33 @@
+III.Test Case - Test Search Product
+
+ID: 003
+
+Title: Search and add the product to the cart on the web page of the shop CENTRUMAMIS.PL
+
+Environment: Chrome version 80.0.3987.163, PyCharm 2019.3.3
+Precondition: Open browser. The user is not logged.
+
+STEPS:
+
+1. Open web page „https://centrumamis.pl/pl”
+2. Click in the "SEARCH" field
+3. Input keyword "WAGA" 
+4. Click "SEARCH BUTTON"
+5. Select first found product
+6. Add first found product in the cart
+7. Get "ALERT MESSAGE ABOUT ADDING THE PRODUCT TO THE CART"(Alert message is: "Produkt dodany do koszyka.")
+8. Confirm taht ALERT MESSAGE displayed correctly on the web page
+9. Close browser
+
+Result:
+
+Searching product and adding to the cart on the web page of the shop works in good way. 
+Alert message with information that product added in cart appers correctly. 
+
+Expected result(behaviour):
+
+After searching and adding product in cart should be appears appropriate Alert message with information that product added in cart. 
+
+Summary:
+
+Searching product and adding in cart works appropriate. Alert message appeared correctly. 
