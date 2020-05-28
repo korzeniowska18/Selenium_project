ade75376620ddb0defb0c881abd231801aaab4aa
korzeniowska18 <57856955+korzeniowska18@users.noreply.github.com>
2020-05-28 11:52:51 +0200

    update xpath

--- 3_page_object_pattern/pages/amis_home_page.py
+++ 3_page_object_pattern/pages/amis_home_page.py
@@ -3,7 +3,7 @@ class AmisHomePage:
     def __init__(self, driver):
         self.driver = driver
         self.search_input_name = 'search'
-        self.search_button_xpath = '/html/body/div[1]/header/div[2]/div[3]/form/div[1]/div[3]/button'
+        self.search_button_xpath = '//div[@class="search__input-area-item"]'
 
     def search_in_amis(self, text):
         self.driver.find_element_by_name(self.search_input_name).send_keys(text)
