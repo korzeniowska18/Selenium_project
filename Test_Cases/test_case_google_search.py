64f9628cb2a85eb6bf694f0f91796773afcb8c1f
korzeniowska18 <57856955+korzeniowska18@users.noreply.github.com>
2020-05-28 07:13:02 +0200

    create test case

---
+++ Test_Cases/test_case_google_search.py
@@ -0,0 +1,37 @@
+II.Test Case - Test Google Search
+
+ID: 002
+
+Title: Search in GOOGLE.COM using keyword and open first link
+Environment: Chrome version 80.0.3987.163, PyCharm 2019.3.3
+
+Precondition: Open browser. 
+
+STEPS:
+
+1. Open web page „https:google.com”
+2. Click in the "SEARCH" field
+3. Input keyword "APPLE" 
+4. Click "SEARCH BUTTON"
+5. Select first found link
+6. Open this page
+7. Get PAGE TITLE - "Apple(Polska)"
+8. Assert PAGE TITLE
+9. Confirm that assertion done correctly
+10. Close browser
+
+ID: 002
+
+Title: Search in GOOGLE.COM using keyword and open first link
+
+Result:
+
+First found link opens correctly. Assertion done.
+
+Expected result(behaviour):
+
+First found web page in Google searching has correct Assertion about page title. 
+
+Summary:
+
+Searching in Google.com works correctly and found web page has correct page title.
